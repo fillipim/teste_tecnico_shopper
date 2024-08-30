0a04f37bad6b0035de09619c0b3ca2ad638f4cd1
Filipe <filipemonferna12@gmail.com>
2024-08-30 18:05:22 -0300

    fix: fixed code details

--- src/routes/readingRoutes.ts
+++ src/routes/readingRoutes.ts
@@ -5,12 +5,8 @@ import { readingsContorller } from "../controllers/readingsController";
 import { validateComfirmDataMiddleware } from "../middlewares/validateConfirmMiddleware";
 import { validateMeasureType } from "../middlewares/validateMeasureTypeMiddleware.ts";
 import { validateUploadDataMiddleware } from "../middlewares/validateUploadMiddleware";
-import NodeCache from "node-cache";
-import path from "path";
 
 export const readingRoutes = Router();
-
-const cache = new NodeCache({ stdTTL: 3600 });
 
 readingRoutes.post("/upload", validateUploadDataMiddleware, readingsContorller);
 readingRoutes.patch(
